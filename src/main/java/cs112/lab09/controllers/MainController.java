1f5ba3c67f3235f82953fdce40291aff2b5c52bc
Zo1nkz <zachpcolinco@gmail.com>
2024-11-30 02:19:06 -0800

    view 1 done; MapView -> MapController

--- src/main/java/cs112/lab09/controllers/MainController.java
+++ src/main/java/cs112/lab09/controllers/MainController.java
@@ -2,13 +2,17 @@ package cs112.lab09.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView logoImageView;
+
+    @FXML
+    public void initialize(){
+        logoImageView.setImage(new Image(getClass().getResourceAsStream("/images/Red-Summer.jpg")));
     }
+
 }
