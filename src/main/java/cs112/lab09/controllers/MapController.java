da457b4eb91f88d825bf15b9d48a052031a6ffe8
Zo1nkz <zachpcolinco@gmail.com>
2024-11-30 02:40:42 -0800

    12:25:00

--- src/main/java/cs112/lab09/controllers/MapController.java
+++ src/main/java/cs112/lab09/controllers/MapController.java
@@ -1,4 +1,15 @@
 package cs112.lab09.controllers;
 
+import javafx.fxml.FXML;
+
 public class MapController {
+    @FXML
+    protected void SanFranciscoCAButtonClick(){
+
+    }
+
+    @FXML
+    protected void BisbeeAZButtonClick(){
+
+    }
 }
