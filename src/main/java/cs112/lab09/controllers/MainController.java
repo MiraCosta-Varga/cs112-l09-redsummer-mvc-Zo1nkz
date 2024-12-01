7a671dcd79507462e6e52f97f46d25ee6dd2fc56
Zo1nkz <zachpcolinco@gmail.com>
2024-11-30 22:08:28 -0800

    Lab 9 Complete

--- src/main/java/cs112/lab09/controllers/MainController.java
+++ src/main/java/cs112/lab09/controllers/MainController.java
@@ -1,18 +1,55 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.MainApplication;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class MainController {
 
     @FXML
     private ImageView logoImageView;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button quitButton;
 
     @FXML
     public void initialize(){
         logoImageView.setImage(new Image(getClass().getResourceAsStream("/images/Red-Summer.jpg")));
+    }
+
+    @FXML
+    protected void onStartButtonClick(ActionEvent actionEvent) throws IOException {
+        //get the map view as a scene
+        Parent mapView = FXMLLoader.load(MainApplication.class.getResource("map-view.fxml"));
+        Scene mapViewScene = new Scene(mapView);
+
+        //get stage
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+        //set the scene (change it) for the stage (window)
+        window.setScene(mapViewScene);
+
+        //show
+        window.show();
+    }
+
+    @FXML
+    protected void onQuitButtonClick(){
+        Platform.exit();
     }
 
 }
--- src/main/java/cs112/lab09/controllers/MapController.java
+++ src/main/java/cs112/lab09/controllers/MapController.java
@@ -1,15 +1,60 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.MainApplication;
+import cs112.lab09.models.Date;
+import cs112.lab09.models.RevisedHistoricalEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MapController {
-    @FXML
-    protected void SanFranciscoCAButtonClick(){
 
+    //Convenient helper method
+    private void openPopupWindow(String location, RevisedHistoricalEvent rhEvent) throws IOException {
+        //get FXML file for the popup window
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApplication.class.getResource("event-view.fxml"));
+        Parent evenViewParent = loader.load();
+
+        //pass data to controller for that specific view
+        EventController eventController = loader.getController();
+        eventController.initData(location, rhEvent);
+
+
+        //create new stage (stage=window)
+        Stage popupStage = new Stage();
+        //extra step: change modality of window
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        //set scene
+        popupStage.setScene(new Scene(evenViewParent));
+        //show scene
+        popupStage.show();
+    }
+    @FXML
+    protected void onSanFranciscoCAButtonClick() throws IOException {
+        RevisedHistoricalEvent sfEvent = new RevisedHistoricalEvent("A minor riot between Black and white soldiers occured in the Presidio in San Francisco.",
+                new Date(5,15,1919), "A minor riot between Black and white soldiers occured in the Presidio in San Francisco after a dispute about a Thai soldier who was moved from the \"colored quarters\" to the white military housing. Despite defending our country, housing for Black soldiers was not only segregated, but often inferior. Black soldiers also faced increased attacks and other forms of discrimination upon returning home.",
+                "https://visualizingtheredsummer.com/?page_id=6");
+
+        this.openPopupWindow("San Francisco, CA", sfEvent);
+
+        //get FXML file for the popup window
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApplication.class.getResource("event-view.fxml"));
+        Parent evenViewParent = loader.load();
     }
 
     @FXML
-    protected void BisbeeAZButtonClick(){
-
+    protected void onBisbeeAZButtonClick() throws IOException {
+        RevisedHistoricalEvent bisbee = new RevisedHistoricalEvent("The Chief of Police and his men began moving through town, systematically disarming Black soldiers by force. Hundreds of shots were fired at the cavalrymen by the police officers and civilian white vigilantes.",
+                new Date(7, 13, 1919),
+                "On July 3, 1919, active members of the Army’s segregated 10th Cavalry Regiment (“Buffalo Soldiers”) were in Bisbee, Arizona, to participate in the town’s Independence Day parade.  According to a New York Times report about the violence in Bisbee, local white law enforcement “planned deliberately to aggravate the Negro troops so that they would furnish an excuse for police and deputy sheriffs to shoot them down.”",
+                "https://www.zinnedproject.org/news/tdih/brewery-gulch-battle/");
+        this.openPopupWindow("Bisbee, AZ", bisbee);
     }
 }
